33368e604bd6bc93589d89708dd92c1213c7cf12
Drian Kars <secnam@secnam.com>
2018-03-28 21:11:51 +0200

    Updated from main

--- install.sh
+++ install.sh
@@ -3,6 +3,9 @@
 sudo apt-get update
 sudo apt-get -y upgrade
 sudo apt-get -y dist-upgrade
+
+sudo apt-get update
+sudo apt-get -y upgrade
 sudo apt-get -y autoremove
 
 sudo cp dhclient.conf /etc/dhcp
